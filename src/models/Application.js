65f8b707dc7420a326dc3e331bc3689038344e70
sam0127-jan <mohiteaniket248@gmail.com>
2026-06-01 09:28:03 +0530

    Fix models and deployment

---
+++ src/models/Application.js
@@ -0,0 +1,23 @@
+
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    phone: String,
+    college: String,
+    branch: String,
+    domain: String,
+    message: String,
+    status: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Application ||
+  mongoose.model("Application", applicationSchema);
